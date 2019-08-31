4fdd1328cdcaf22ed676f119e3a4a4e330771778
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-08-31 18:57:03 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,14 +1,14 @@
 class Vehicle
   
-  attr_accessor :wheel_size, :number
+  attr_accessor :wheel_size, :wheel_number
   
-  def initialize(wheel_size, number)
+  def initialize(wheel_size, wheel_number)
     @wheel_size = wheel_size
-    @number = number
+    @wheel_number = wheel_number
   end 
   
   def go 
-    "vrrrrrrrrooom!"
+    "vrrrrrrrooom!"
   end 
   
   def fill_up_tank
